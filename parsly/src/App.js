d4028cb302363fa12df092125a7720aa92941a49
Albin Matson Gyllang <albingyllang@gmail.com>
2022-10-06 14:31:35 +0200

    Fetch data
    Fetch historical data from weatherbit.io API

--- parsly/src/App.js
+++ parsly/src/App.js
@@ -1,12 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
 
-function App() {
+const API_KEY = process.env.REACT_APP_API_KEY;
+// console.log("api key", API_KEY);
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  console.log("state data:", data);
+
+  useEffect(() => {
+    const fetchWeatherData = async (lat = 59.3294, lon = 18.0686) => {
+      // var url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${API_KEY}&include=minutely`;
+      var url = `https://api.weatherbit.io/v2.0/history/daily?lat=${lat}&lon=${lon}&start_date=2021-06-01&end_date=2021-06-20&key=${API_KEY}`;
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        let data = await response.json();
+        setData(data.data);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeatherData();
+  }, []);
   return (
     <div className="App">
-     Hello world
+      <NavBar />
+      Hello world
     </div>
   );
-}
+};
 
 export default App;
---
+++ parsly/src/components/navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div className="navbar font1"> Parsley Cultivation Dashboard</div>;
+};
+
+export default NavBar;
